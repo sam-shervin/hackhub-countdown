b95cc985529ade913a85806fe8cd2108db726f66
sam-shervin <samshervin.sebastian@gmail.com>
2025-03-09 08:49:41 +0530

    refactor layout in page.tsx for improved responsiveness and structure

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,9 @@ import FlipClock from '@/components/Timer/Timer';
 
 export default function BgImage() {
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
-      <Image
-        src="/Title.png" // update with your image path
-        alt="Background Image"
-        layout="fill"
-        objectFit="contain"
-        priority
-      />
-      <div style={{ position: 'absolute', zIndex: 1 }} className='bottom-0 mx-auto w-full'>
-        <section className='w-[90vw] mx-auto scale-80 -mb-12'>
+    <div className="flex flex-col justify-center text-[1.3vw]  items-center" style={{ position: 'relative', width: '100%', height: '100vh' }}>
+      <div style={{zIndex: 1 }} className=' mx-auto w-full'>
+        <section className='w-[98vw] mx-auto -mb-12'>
         <FlipClock /></section>
       </div>
     </div>
